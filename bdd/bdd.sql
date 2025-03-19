7267d413459d610f8c21b8d557d42034dd7c2621
ZitouneMcGregor <carreregee.olivier@gmail.com>
2025-03-19 09:35:44 +0100

    bdd

--- bdd/bdd.sql
+++ bdd/bdd.sql
@@ -3,23 +3,28 @@ CREATE DATABASE IF NOT EXISTS FilmDerDataBase;
 
 USE FilmDerDataBase;
 
-CREATE TABLE IF NOT EXISTS Utilisateur (
+CREATE TABLE IF NOT EXISTS User (
     id INT AUTO_INCREMENT PRIMARY KEY,  
-    Utilisateurname VARCHAR(50) NOT NULL,    
+    pseudo VARCHAR(50) NOT NULL,    
     password VARCHAR(255) NOT NULL    
 );
 
+
 CREATE TABLE IF NOT EXISTS Room(
     id INT AUTO_INCREMENT PRIMARY KEY
+    id_admin INT,
+    
+    FOREIGN KEY (id_admin) REFERENCES User(id)
 );
 
 
-
-CREATE TABLE IF NOT EXISTS UtilisateurRoom(
+CREATE TABLE IF NOT EXISTS UserRoom(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    Utilisateur_id INT NOT NULL,
+    user_id INT NOT NULL,
     room_id INT NOT NULL,
-    FOREIGN KEY (Utilisateur_id) REFERENCES Utilisateur(id),
+    index_film INT DEFAULT 0,
+    
+    FOREIGN KEY (user_id) REFERENCES User(id),
     FOREIGN KEY (room_id) REFERENCES Room(id)
 );
 
@@ -28,24 +33,31 @@ CREATE TABLE IF NOT EXISTS RoomMovie(
     id INT AUTO_INCREMENT PRIMARY KEY,
     room_id INT NOT NULL,
     movie_id INT NOT NULL,
+    
     FOREIGN KEY (room_id) REFERENCES Room(id),
 );
 
-CREATE TABLE IF NOT EXISTS UtilisateurMovie(
+
+CREATE TABLE IF NOT EXISTS UserMovie(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    Utilisateur_id INT NOT NULL,
+    user_id INT NOT NULL,
     movie_id INT NOT NULL,
-    FOREIGN KEY (Utilisateur_id) REFERENCES Utilisateur(id),
+    movie_img VARCHAR(255), --faut mettre plus
+    movie_rating INT,
+    movie_name VARCHAR(255),
+    
+    FOREIGN KEY (user_id) REFERENCES User(id),
 );
 
 
 CREATE TABLE IF NOT EXISTS Result(
     id INT AUTO_INCREMENT PRIMARY KEY,
     res VARCHAR(50) NOT NULL,
-    Utilisateur_id INT NOT NULL,
+    user_id INT NOT NULL,
     room_id INT NOT NULL,
     movie_id INT NOT NULL,
-    FOREIGN KEY (Utilisateur_id) REFERENCES Utilisateur(id),
+    
+    FOREIGN KEY (user_id) REFERENCES User(id),
     FOREIGN KEY (room_id) REFERENCES Room(id),
 );
 
