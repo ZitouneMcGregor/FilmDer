47eb6bf909a3e3e6e8fd4aeac5cf79b163e194d8
ZitouneMcGregor <carreregee.olivier@gmail.com>
2025-03-29 16:18:29 +0100

    guard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -9,15 +9,16 @@ import { LoginGuard } from './guards/loginGuard/login-guard.guard';
 import { RommPlayComponent } from './components/room/romm-play/romm-play.component';
 import { ProfilSkeletonComponent } from './components/profil/profil-skeleton/profil-skeleton.component';
 import { RommSkeletonComponent } from './components/room/romm-skeleton/romm-skeleton.component';
+import { RoomGuard } from './guards/roomGuard/room-guard.guard';
 
 
 export const routes: Routes = [
-    { path: 'home', component: HomeSkeletonComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: LoginSkeletonComponent, canActivate: [LoginGuard]},
-    { path: 'play', component: PlaySkeletonComponent, canActivate: [AuthGuard]},
-    { path: 'films', component: FilmsSkeletonComponent, canActivate: [AuthGuard]},
-    { path: 'profil', component: ProfilSkeletonComponent, canActivate: [AuthGuard]},
-    { path: 'room/:id', component: RommSkeletonComponent, canActivate:  [AuthGuard]},
+    { path: 'home', component: HomeSkeletonComponent, canActivate: [ AuthGuard ] },
+    { path: 'login', component: LoginSkeletonComponent, canActivate: [ LoginGuard ]},
+    { path: 'play', component: PlaySkeletonComponent, canActivate: [ AuthGuard ]},
+    { path: 'films', component: FilmsSkeletonComponent, canActivate: [ AuthGuard ]},
+    { path: 'profil', component: ProfilSkeletonComponent, canActivate: [ AuthGuard ]},
+    { path: 'room/:id', component: RommSkeletonComponent, canActivate:  [ AuthGuard, RoomGuard ]},
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
     { path: '**', component: NotFoundComponent }
     
