cdac22fbccf8ad42add5442d74e3a7b66041e86f
ZitouneMcGregor <carreregee.olivier@gmail.com>
2025-03-29 23:04:13 +0100

    header

--- frontend/environment.ts
+++ frontend/environment.ts
@@ -2,10 +2,3 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:8000'
 };
-  
-
-// pas mis en place encore
-var jetonZit = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYzE0N2IyZjMxMTVmOTNkNjQ2YjdkM2I2ZGE3M2I4OSIsIm5iZiI6MTc0MjQ2MjI4MS4yODcwMDAyLCJzdWIiOiI2N2RiZGQ0OWZlYzU4YWEzYzVlOWVlYzYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.NH_SIV66JTLKcXRfZlwF_b7_eobMrvW44OfXdsO_xuU"
-var cléZit = "1c147b2f3115f93d646b7d3b6da73b89"
-
-var pathTmdbImg = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/"
